b6dbc504928413cb901d3e13ea2c2a44752393e1
David Kanwisher <kanwisher@gmail.com>
2017-02-07 22:01:35 -0500

    Google login?

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,6 +16,35 @@ let timer = window.setInterval(updateAll, 5 * 1000); //update information every 
 
   let database = firebase.database();
 
+var provider = new firebase.auth.GoogleAuthProvider();
+
+
+
+
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+$("#test").on('click', function(){
+    firebase.auth().signInWithRedirect(provider);
+  });
+
+
 
 
 updateAll(); //display info on initial page load
